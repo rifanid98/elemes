774d7f26cc6cd136e514c1e3263dc7971186ec03
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2021-11-24 20:57:22 +0700

    course entity

---
+++ src/domain/entity/course.entity.ts
@@ -0,0 +1,23 @@
+export interface CourseEntityInterface {
+  id?: number;
+  name?: string;
+  description?: string;
+  rating?: number;
+  bought?: number;
+  price?: number;
+  created_at?: Date;
+  updated_at?: Date;
+  deleted_at?: Date;
+}
+
+export class Course implements CourseEntityInterface {
+  id?: number;
+  name?: string;
+  description?: string;
+  rating?: number;
+  bought?: number;
+  price?: number;
+  created_at?: Date;
+  updated_at?: Date;
+  deleted_at?: Date;
+}
---
+++ src/infrastructure/persistence/local/typeorm/entity/course.entity.ts
@@ -0,0 +1,60 @@
+import { CourseEntityInterface } from 'domain/entity/course.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Course implements CourseEntityInterface {
+  @PrimaryGeneratedColumn('increment')
+  id?: number;
+
+  @Column({
+    type: 'varchar',
+    length: '50',
+    nullable: true,
+  })
+  name?: string;
+
+  @Column({
+    type: 'varchar',
+    length: '100',
+    nullable: true,
+    unique: true,
+  })
+  description?: string;
+
+  @Column({
+    type: 'int',
+    nullable: true,
+    unique: true,
+  })
+  rating?: number;
+
+  @Column({
+    type: 'int',
+    nullable: true,
+    unique: true,
+  })
+  bought?: number;
+
+  @Column({
+    type: 'int',
+    nullable: true,
+    unique: true,
+  })
+  price?: number;
+
+  @CreateDateColumn()
+  created_at?: Date;
+
+  @UpdateDateColumn()
+  updated_at?: Date;
+
+  @DeleteDateColumn()
+  deleted_at?: Date;
+}
