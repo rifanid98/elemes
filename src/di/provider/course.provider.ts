9db9d448f49474060ed1c0671eec4d94c5465c87
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2021-11-25 23:27:30 +0700

    add File dependency

--- src/di/provider/course.provider.ts
+++ src/di/provider/course.provider.ts
@@ -1,6 +1,7 @@
 import { Provider } from '@nestjs/common';
 import { CourseInteractor } from 'usecase/course/course.interactor';
 import { CoursePresenter } from 'adapter/presenter/course.presenter';
+import { File } from 'src/sharedkernel/file';
 
 export const CourseProvider: Provider[] = [
   {
@@ -10,5 +11,9 @@ export const CourseProvider: Provider[] = [
   {
     provide: 'CoursePresenterInterface',
     useClass: CoursePresenter,
+  },
+  {
+    provide: File,
+    useClass: File,
   },
 ];
