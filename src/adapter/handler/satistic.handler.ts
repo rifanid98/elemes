2ff0cb7d91863a47e201efa4c48b2554970362aa
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2021-11-25 20:46:11 +0700

    set authentication guards

--- src/adapter/handler/satistic.handler.ts
+++ src/adapter/handler/satistic.handler.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, Inject, UseFilters } from '@nestjs/common';
+import { Controller, Get, Inject, UseFilters, UseGuards } from '@nestjs/common';
 import {
   Statistic,
   StatisticUsecase,
 } from 'usecase/statistic/statistic.usecase';
 import { QueryExceptionFilter } from 'sharedkernel/nest/exception-filter';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { StatisticResponseBody } from 'src/infrastructure/openapi/schema';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('statistics')
+@UseGuards(AuthGuard('jwt'))
 @ApiTags('Statistic')
+@ApiBearerAuth('Authorization')
 export class StatisticHandler {
   constructor(@Inject('StatisticUsecase') private useCase: StatisticUsecase) {}
 
