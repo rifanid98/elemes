4824b0385bac1890b1f6e0c4cec9fa6fd8b09fad
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2021-11-25 23:26:51 +0700

    add updateCourseImage() method

--- src/adapter/handler/course.handler.ts
+++ src/adapter/handler/course.handler.ts
@@ -9,8 +9,10 @@ import {
   Patch,
   Post,
   Query,
+  UploadedFile,
   UseFilters,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CourseUsecase } from 'usecase/course/course.usecase';
 import { QueryExceptionFilter } from 'sharedkernel/nest/exception-filter';
@@ -18,6 +20,7 @@ import {
   ApiBearerAuth,
   ApiBody,
   ApiConflictResponse,
+  ApiConsumes,
   ApiCreatedResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
@@ -28,6 +31,7 @@ import {
 import { Course } from 'domain/entity/course.entity';
 import {
   CourseConflictResponseBody,
+  CourseFileRequestBody,
   CourseFilter,
   CourseNotFoundResponse,
   CourseRequestBody,
@@ -37,6 +41,8 @@ import { CourseDto, CourseFilterDto } from 'domain/dto/course.dto';
 import { RolesGuard } from 'src/sharedkernel/nest/guard';
 import { Role, Roles } from 'src/sharedkernel/nest/decorator';
 import { AuthGuard } from '@nestjs/passport';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { imageMulterOptions } from 'src/sharedkernel/nest/common-config';
 
 @Controller('courses')
 @UseGuards(AuthGuard('jwt'))
@@ -136,7 +142,32 @@ export class CourseHandler {
     @Param('id', ParseIntPipe) id: number,
     @Body() course: CourseDto,
   ): Promise<boolean> {
+    console.log(course);
     course.id = id;
+    return this.useCase.updateCourse(course);
+  }
+
+  @Patch('/:id/image')
+  @Roles(Role.Admin, Role.SuperAdmin)
+  @UseGuards(RolesGuard)
+  @UseInterceptors(FileInterceptor('file', imageMulterOptions))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({ type: CourseFileRequestBody })
+  @ApiOperation({ summary: 'Update course image' })
+  @ApiOkResponse({
+    description: 'Course image updated',
+  })
+  @ApiNotFoundResponse({
+    type: CourseNotFoundResponse,
+    description: 'Course does not exists',
+  })
+  updateCourseImage(
+    @Param('id', ParseIntPipe) id: number,
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<boolean> {
+    const course = new CourseDto();
+    course.id = id;
+    course.file = file;
     return this.useCase.updateCourse(course);
   }
 
