02f8de723368e65483587e6839a4023fb21f88da
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2021-11-26 23:30:21 +0700

    update seeder

--- src/infrastructure/persistence/local/typeorm/seeder/data/user.data.ts
+++ src/infrastructure/persistence/local/typeorm/seeder/data/user.data.ts
@@ -2,6 +2,11 @@ import { User } from '../../entity/user.entity';
 
 export const users: User[] = [
   {
+    name: 'staff',
+    email: 'staff@email.com',
+    password: '$2b$10$rYGrc5GwG6qrZM43ZHk5yuBBW3uN6wPFHGcxht0XzVcMMZyG9i0IO',
+  },
+  {
     name: 'admin',
     email: 'admin@email.com',
     password: '$2b$10$rYGrc5GwG6qrZM43ZHk5yuBBW3uN6wPFHGcxht0XzVcMMZyG9i0IO',
