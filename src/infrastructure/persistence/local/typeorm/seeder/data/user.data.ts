bf15f2fb870fac772af9e7352125fa0071318881
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2021-11-26 15:57:42 +0700

    update data

--- src/infrastructure/persistence/local/typeorm/seeder/data/user.data.ts
+++ src/infrastructure/persistence/local/typeorm/seeder/data/user.data.ts
@@ -1,24 +1,9 @@
-import { User } from 'domain/entity/user.entity';
+import { User } from '../../entity/user.entity';
 
 export const users: User[] = [
   {
-    name: 'user',
-    email: 'user@email.com',
-    password: '$2b$10$rYGrc5GwG6qrZM43ZHk5yuBBW3uN6wPFHGcxht0XzVcMMZyG9i0IO',
-  },
-  {
-    name: 'Peppy Minarosa',
-    email: 'Peppy Minarosa@mail.com',
-    password: '$2b$10$rYGrc5GwG6qrZM43ZHk5yuBBW3uN6wPFHGcxht0XzVcMMZyG9i0IO',
-  },
-  {
-    name: 'Diba Melati Putri',
-    email: 'Diba Melati Putri@mail.com',
-    password: '$2b$10$rYGrc5GwG6qrZM43ZHk5yuBBW3uN6wPFHGcxht0XzVcMMZyG9i0IO',
-  },
-  {
-    name: 'Dedy Kamal',
-    email: 'Dedy Kamal@mail.com',
+    name: 'admin',
+    email: 'admin@email.com',
     password: '$2b$10$rYGrc5GwG6qrZM43ZHk5yuBBW3uN6wPFHGcxht0XzVcMMZyG9i0IO',
   },
 ];
