a2f3c571b32b44493471992d7be32e8a3bc6e02f
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2021-11-27 00:03:03 +0700

    update cloudinary config

--- src/di/provider/cloudinary.provider.ts
+++ src/di/provider/cloudinary.provider.ts
@@ -5,9 +5,9 @@ export const CloudinaryProvider = {
   useFactory: (): ConfigOptions => {
     // TODO: put cloudinary stuff in .env
     return Cloudinary.config({
-      cloud_name: 'your_cloud_name',
-      api_key: 'your_api_key',
-      api_secret: 'your_api_secret',
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
     });
   },
 };
