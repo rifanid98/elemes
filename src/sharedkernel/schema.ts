2e4b35bd7fb8565c5d6ada8b56040f3b8283c347
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2021-11-26 13:37:55 +0700

    add env variables

--- src/sharedkernel/schema.ts
+++ src/sharedkernel/schema.ts
@@ -1,6 +1,7 @@
 import * as Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
+  PORT: Joi.number().default(3000),
   TYPEORM_CONNECTION: Joi.string().required(),
   TYPEORM_HOST: Joi.string().required(),
   TYPEORM_USERNAME: Joi.string().required(),
@@ -11,4 +12,8 @@ export const configValidationSchema = Joi.object({
   TYPEORM_LOGGING: Joi.string().required(),
   JWT_SECRET: Joi.string().required(),
   AUTHENTICATOR_NAME: Joi.string().required(),
+  CLOUDINARY_CLOUD_NAME: Joi.string().required(),
+  CLOUDINARY_API_KEY: Joi.string().required(),
+  CLOUDINARY_API_SECRET: Joi.string().required(),
+  CLOUDINARY_URL: Joi.string().required(),
 });
